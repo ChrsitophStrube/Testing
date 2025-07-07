f8c1145fcfaa521023192e411d04e8ebde913d75
Sven Sprey <s.sprey@randompixel-games.com>
2025-07-07 14:20:17 +0000

    Updated UnitTest1.cs

--- tests/HmiTesting.PlaywrightTests/UnitTest1.cs
+++ tests/HmiTesting.PlaywrightTests/UnitTest1.cs
@@ -33,7 +33,7 @@ public class Tests
         //Open Page
         _page = await _browser.NewPageAsync();
         await _page.SetViewportSizeAsync(1920, 1080);
-        await _page.GotoAsync("http://localhost:8080", new PageGotoOptions
+        await _page.GotoAsync("http://192.168.1.200:50080", new PageGotoOptions
         {
             WaitUntil = WaitUntilState.NetworkIdle
         });
@@ -42,7 +42,7 @@ public class Tests
 
         //conect to OPCUA Server
         OpcUaClient client = new OpcUaClient();
-        _session = (OpcUaSession)client.Connect("MyHMI_Template_Unencrypted");
+        _session = (OpcUaSession)client.Connect("MyHMI_Template_Unencrypted", "192.168.1.200");
     }
 
     [TearDown]
